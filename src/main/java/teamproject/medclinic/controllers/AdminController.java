5d56ee3ee5e43616f9f4dde0a6f3f0d85084b52e
Debora Bastos <65675553+DeboraCobat@users.noreply.github.com>
2023-05-07 16:32:27 -0400

    delete ok. update api working (not html)

--- src/main/java/teamproject/medclinic/controllers/AdminController.java
+++ src/main/java/teamproject/medclinic/controllers/AdminController.java
@@ -1,9 +1,12 @@
 package teamproject.medclinic.controllers;
 
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
 import org.springframework.web.bind.annotation.*;
+import teamproject.medclinic.entity.Appointments;
 import teamproject.medclinic.entity.User;
 import teamproject.medclinic.repository.UserRepo;
 
@@ -26,6 +29,9 @@ public class AdminController {
         model.addAttribute("admin", admin);
         return "admin/dashboard";
     }
+
+
+    //// DOCTOR CRUD ////
 
     @GetMapping ("/doctorsList")
     public String doctorsList(Model model) {
@@ -60,8 +66,88 @@ public class AdminController {
         return "admin/doctorUpdate";
     }
 
-    @PostMapping("/doctorUpdate/{id}")
-    public String doctorUpdate(@PathVariable("id") Long id, @ModelAttribute("user") User updatedUser) {
+    @PostMapping("/updateDoctor/{id}")
+    public String updateDoctor(@PathVariable("id") Long id, @ModelAttribute("user") User user) {
+        User existingDoctor = userRepo.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+        existingDoctor.setFirstName(user.getFirstName());
+        existingDoctor.setLastName(user.getLastName());
+        existingDoctor.setDateOfBirth(user.getDateOfBirth());
+        existingDoctor.setAddress(user.getAddress());
+        existingDoctor.setPhoneNumber(user.getPhoneNumber());
+        existingDoctor.setEmail(user.getEmail());
+        existingDoctor.setSpecialty(user.getSpecialty());
+
+        System.out.println(user);
+        System.out.println(existingDoctor);
+
+        userRepo.save(existingDoctor);
+
+        return "redirect:/admin/doctorsList";
+    }
+
+//    @PutMapping("/updateDoctor/{id}")
+//    public ResponseEntity<User> updateDoctor(@PathVariable(value = "id") Long id,
+//                                             @Valid @ModelAttribute("user") User user){
+//        User existingDoctor = userRepo.findById(id)
+//                .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+//        existingDoctor.setFirstName(user.getFirstName());
+//        existingDoctor.setLastName(user.getLastName());
+//        existingDoctor.setDateOfBirth(user.getDateOfBirth());
+//        existingDoctor.setAddress(user.getAddress());
+//        existingDoctor.setPhoneNumber(user.getPhoneNumber());
+//        existingDoctor.setEmail(user.getEmail());
+//        existingDoctor.setSpecialty(user.getSpecialty());
+//
+//        final User updatedUser = userRepo.save(existingDoctor);
+//        return ResponseEntity.ok(updatedUser);
+//    }
+
+    //Delete Operation
+    @GetMapping("/deleteUser/{id}")
+    public String deleteUser(@PathVariable("id") Long id) {
+        userRepo.deleteById(id);
+        return "redirect:/admin/doctorsList";
+    }
+
+
+    //// PATIENT CRUD ////
+
+    // List ALL Operation
+    @GetMapping("/patientsList")
+    public String patientsList(Model model) {
+        List<User> patients = userRepo.findByRole(User.Role.patient);
+        model.addAttribute("patients", patients);
+        return "admin/patientsList";
+    }
+
+    // Create Operation
+    @GetMapping("/patientCreate")
+    public String patientCreate(Model model) {
+        User user = new User();
+        user.setRole(User.Role.patient);
+        model.addAttribute("user", user);
+        return "admin/patientCreate";
+    }
+
+    @PostMapping("/patientCreate")
+    public String patientCreate(@ModelAttribute("user") User user) {
+        user.setRole(User.Role.patient);
+        userRepo.save(user);
+        return "redirect:/admin/patientsList";
+    }
+
+    // Update Operation
+    @GetMapping("/patientUpdate/{id}")
+    public String patientUpdate(@PathVariable("id") Long id, Model model) {
+        User user = userRepo.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+        model.addAttribute("user", user);
+        return "admin/patientUpdate";
+    }
+
+    @PostMapping("/patientUpdate/{id}")
+    public String patientUpdate(@PathVariable("id") Long id, @ModelAttribute("user") User updatedUser) {
         User existingUser = userRepo.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
         existingUser.setFirstName(updatedUser.getFirstName());
@@ -70,10 +156,25 @@ public class AdminController {
         existingUser.setAddress(updatedUser.getAddress());
         existingUser.setPhoneNumber(updatedUser.getPhoneNumber());
         existingUser.setEmail(updatedUser.getEmail());
-        existingUser.setSpecialty(updatedUser.getSpecialty());
         userRepo.save(existingUser);
-        return "redirect:/admin/doctorsList";
+        return "redirect:/admin/patientsList";
     }
 
+    //Delete Operation
+//    @GetMapping("/deleteUser/{id}")
+//    public String deleteUser(@PathVariable("id") Long id) {
+//        userRepo.deleteById(id);
+//        return "redirect:/admin/patientsList";
+//    }
 
+
+    ///APPOINTMENTS CRUD////
+
+//    @GetMapping("/appointmentsList")
+//    public String appointmentsList(Model model) {
+//        List<Appointments> appointments = appointmentRepo.findAll();
+//        model.addAttribute("appointments", appointments);
+//        return "admin/appointmentsList";
+//    }
 }
+
--- src/main/java/teamproject/medclinic/controllers/PatientController.java
+++ src/main/java/teamproject/medclinic/controllers/PatientController.java
@@ -1,44 +1,44 @@
-package teamproject.medclinic.controllers;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import teamproject.medclinic.entity.User;
-import teamproject.medclinic.repository.UserRepo;
-
-import java.util.List;
-
-@Controller
-public class PatientController {
-    private final UserRepo userRepo;
-
-    public PatientController(UserRepo userRepo) {
-        this.userRepo = userRepo;
-    }
-
-    // List ALL Operation (HTTP GET)
-    @GetMapping("/admin/patientsList")
-    public String patientsList(Model model) {
-        List<User> patients = userRepo.findByRole(User.Role.patient);
-        model.addAttribute("patients", patients);
-        return "admin/patientsList";
-    }
-
-    // Create Operation (HTTP Post)
-    @RequestMapping("/admin/patientCreate")
-    public String patientCreate(Model model) {
-        User user = new User();
-        user.setRole(User.Role.patient);
-        model.addAttribute("user", user);
-        return "admin/patientCreate";
-    }
-
-    @PostMapping("/admin/patientCreate")
-    public String patientCreate(@ModelAttribute("user") User user) {
-        user.setRole(User.Role.patient);
-        userRepo.save(user);
-        return "redirect:/admin/patientsList";
-    }
-}
+//package teamproject.medclinic.controllers;
+//
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.*;
+//import teamproject.medclinic.entity.User;
+//import teamproject.medclinic.repository.UserRepo;
+//
+//import java.util.List;
+//
+//@Controller
+//public class PatientController {
+//    private final UserRepo userRepo;
+//
+//    public PatientController(UserRepo userRepo) {
+//        this.userRepo = userRepo;
+//    }
+//
+//    // List ALL Operation (HTTP GET)
+//    @GetMapping("/admin/patientsList")
+//    public String patientsList(Model model) {
+//        List<User> patients = userRepo.findByRole(User.Role.patient);
+//        model.addAttribute("patients", patients);
+//        return "admin/patientsList";
+//    }
+//
+//    // Create Operation (HTTP Post)
+//    @RequestMapping("/admin/patientCreate")
+//    public String patientCreate(Model model) {
+//        User user = new User();
+//        user.setRole(User.Role.patient);
+//        model.addAttribute("user", user);
+//        return "admin/patientCreate";
+//    }
+//
+//    @PostMapping("/admin/patientCreate")
+//    public String patientCreate(@ModelAttribute("user") User user) {
+//        user.setRole(User.Role.patient);
+//        userRepo.save(user);
+//        return "redirect:/admin/patientsList";
+//    }
+//}
